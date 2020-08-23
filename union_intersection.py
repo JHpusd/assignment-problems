1a23ff8bd334b8267fc0c78b0f46059f21d5b667
JHpusd <justinhongdebate@gmail.com>
2020-08-23 23:03:25 +0000

    shortened union_intersection.py

--- union_intersection.py
+++ union_intersection.py
@@ -1,11 +1,10 @@
 
 
 def intersection(list_1, list_2):
-    intersection_set = set.intersection(set(list_1), set(list_2))
-    intersection_list = list(intersection_set)
+    intersection = list(set(list_1) & set(list_2))
 
-    print(intersection_list)
-    return intersection_list
+    print(intersection)
+    return intersection
 
 print('''Asserting that 'intersection' works on
 [1, 2, 'a', 'b'] and [2, 3, 'a'] ...''')
@@ -16,11 +15,10 @@ print("\n")
 
 
 def union(list_1, list_2):
-    union_set = set.union(set(list_1), set(list_2))
-    union_list = list(union_set)
+    union = list(set(list_1) | set(list_2))
 
-    print(union_list)
-    return union_list
+    print(union)
+    return union
 
 print("Asserting that 'union' works on [1, 2, 'a', 'b'] and [2, 3, 'a'] ...")
 assert union([1, 2, "a", "b"], [2, 3, "a"])
