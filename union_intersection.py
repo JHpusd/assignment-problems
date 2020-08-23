b8d2e3c3ac0fd5eef063715fb310f7573e776e9a
JHpusd <justinhongdebate@gmail.com>
2020-08-23 20:55:22 +0000

    finished problem 2-1

---
+++ union_intersection.py
@@ -0,0 +1,26 @@
+
+
+def intersection(list_1, list_2):
+    intersection_set = set.intersection(set(list_1), set(list_2))
+    intersection_list = list(intersection_set)
+
+    print(intersection_list)
+    return intersection_list
+
+print("Asserting that 'intersection' works on [1, 2, 'a', 'b'] and [2, 3, 'a'] ...")
+assert intersection([1, 2, "a", "b"], [2, 3, "a"]), "doesn't work"
+print("PASSED")
+
+print("\n")
+
+
+def union(list_1, list_2):
+    union_set = set.union(set(list_1), set(list_2))
+    union_list = list(union_set)
+
+    print(union_list)
+    return union_list
+
+print("Asserting that 'union' works on [1, 2, 'a', 'b'] and [2, 3, 'a'] ...")
+assert union([1, 2, "a", "b"], [2, 3, "a"]), "doesn't work"
+print("PASSED")
