27a972c50cbed55c7ae35279f23ba524c2e80ef3
JHpusd <justinhongdebate@gmail.com>
2020-08-24 18:18:16 +0000

    linted recursive sequence

--- recursive_sequence.py
+++ recursive_sequence.py
@@ -10,13 +10,13 @@ def first_n_terms(n):
             return_list.append(5)
         return_list.append(3*previous - 4)
         previous = 3*previous - 4
-    
+
     print(return_list)
     return return_list
 
 print("Asserting that first_n_terms works on input n = 10 ...")
 assert first_n_terms(10) == [
-    5, 11, 29, 83, 245, 731, 
+    5, 11, 29, 83, 245, 731,
     2189, 6563, 19685, 59051], "didn't work"
 print("PASSED")
 
