ae2b3d3fc72c938c33eb227bea0452bb448e255d
JHpusd <justinhongdebate@gmail.com>
2020-09-20 18:16:09 +0000

    fixed monte_carlo flips for assignment 11

--- assignment_7/monte_carlo_coin_flips.py
+++ assignment_7/monte_carlo_coin_flips.py
@@ -2,7 +2,7 @@ from random import random
 
 
 def factorial(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return 1
     return n * factorial(n-1)
 
@@ -12,28 +12,30 @@ def probability(num_heads, num_flips):
     total_outcomes = 2 ** num_flips
     desired_outcomes = factorial(num_flips) / (
         factorial(num_heads)*factorial(num_flips - num_heads))
-    return round((desired_outcomes / total_outcomes), 5)
+    return round((desired_outcomes / total_outcomes), 6)
 
+'''
 print("Asserting that 'probability' works...")
 assert probability(5, 8) == 0.21875
 print("PASSED")
 print("\n")
-
+'''
 
 def monte_carlo_probability(num_heads, num_flips):
-    for j in range(5):
-        success = 0
-        for i in range(1000):
-            flip_list = []
-            for n in range(num_flips):
-                flip_result = round(random())
-                if flip_result == 0:
-                    flip_list.append("H")
-                else:
-                    flip_list.append("T")
-            if flip_list.count("H") == num_heads:
-                success += 1
-        print(success/1000)
-
+    success = 0
+    for i in range(1000):
+        flip_list = []
+        for n in range(num_flips):
+            flip_result = round(random())
+            if flip_result == 0:
+                flip_list.append("H")
+            else:
+                flip_list.append("T")
+        if flip_list.count("H") == num_heads:
+            success += 1
+    return (success/1000)
+'''
 print("Monte Carlo probabilities for 5 heads out of 8 flips:")
-monte_carlo_probability(5, 8)
+for i in range(5):
+    print(monte_carlo_probability(5, 8))
+'''
