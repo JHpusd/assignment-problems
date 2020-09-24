1755c1a44a848fb3ef8ab202e3f926b233ebbc7b
JHpusd <justinhongdebate@gmail.com>
2020-09-24 22:48:56 +0000

    finished 12-1 (tally sort)

---
+++ assignment_12/tally_sort.py
@@ -0,0 +1,38 @@
+def min_value(num_list):
+    smallest = num_list[0]
+    for num in num_list:
+        if num < smallest:
+            smallest = num
+    return smallest
+
+def max_value(num_list):
+    largest = num_list[0]
+    for num in num_list:
+        if num > largest:
+            largest = num
+    return largest
+
+def tally_sort(num_list):
+    smallest = min_value(num_list)
+    for n in range(len(num_list)):
+        num_list[n] -= smallest
+
+    tally_list = []
+    for n in range(max_value(num_list) + 1):
+        tally_list.append(0)
+    
+    for num in num_list:
+        tally_list[num] += 1
+
+    result_list = []
+    for n in range(len(tally_list)):
+        for _ in range(tally_list[n]):
+            result_list.append(n)
+    
+    for n in range(len(result_list)):
+        result_list[n] += smallest
+    return result_list
+
+print("Asserting that 'tally_sort' works...")
+assert tally_sort([2, 5, 2, 3, 8, 6, 3]) == [2, 2, 3, 3, 5, 6, 8]
+print("PASSED")
---
+++ test.py
@@ -0,0 +1,4 @@
+list1 = []
+for i in range(0,0):
+    list1.append(0)
+print(list1)
