d8327ee0b59bba1644ba9c5577d9da37e89108b3
JHpusd <justinhongdebate@gmail.com>
2020-10-11 19:05:58 +0000

    updated node class to have index, push, and get node

--- assignment_16/linked_list.py
+++ assignment_16/linked_list.py
@@ -3,6 +3,7 @@ class Node():
     def __init__(self, data, next):
         self.data = data
         self.next = next
+        self.index = 0
 
 class LinkedList():
     def __init__(self, head):
@@ -26,9 +27,26 @@ class LinkedList():
     
     def append(self, next_node):
         ref_node = self.head
+        index_counter = 0
         while ref_node.next != None:
             ref_node = ref_node.next
+            index_counter += 1
         ref_node.next = Node(next_node, None)
+        ref_node.next.index = index_counter + 1
+    
+    def push(self, new_data):
+        ref_node = self.head
+        self.head = Node(new_data, None)
+        while ref_node.next != None:
+            self.append(ref_node.data)
+            ref_node = ref_node.next
+        self.append(ref_node.data)
+    
+    def get_node(self, index):
+        ref_node = self.head
+        for i in range(index):
+            ref_node = ref_node.next
+        return ref_node.data
 
 print("Testing class 'Node()'...")
 A = Node(4, None)
@@ -48,4 +66,25 @@ assert linked_list.head.next.data == 8
 linked_list.append(9)
 linked_list.print_data()
 assert linked_list.length() == 3
+print("PASSED")
+print("\n")
+
+print("Testing methods 'length' and 'push'...")
+linked_list = LinkedList('b')
+linked_list.append('e')
+linked_list.append('f')
+linked_list.push('a')
+assert linked_list.length() == 4
+print("PASSED")
+print("Testing attribute 'index'...")
+assert linked_list.head.index == 0
+assert linked_list.head.next.index == 1
+assert linked_list.head.next.next.index == 2
+assert linked_list.head.next.next.next.index == 3
+print("PASSED")
+print("Testing method 'get_node'...")
+assert linked_list.get_node(0) == 'a'
+assert linked_list.get_node(1) == 'b'
+assert linked_list.get_node(2) == 'e'
+assert linked_list.get_node(3) == 'f'
 print("PASSED")
