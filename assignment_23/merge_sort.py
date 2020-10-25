d2fd292aa61f476da46621274e62b0d2c4b04a51
JHpusd <justinhongdebate@gmail.com>
2020-10-25 16:31:57 +0000

    fixed merge_sort

--- assignment_23/merge_sort.py
+++ assignment_23/merge_sort.py
@@ -1,12 +1,20 @@
-def sort(num_list):
+def sort(x, y):
     result_list = []
-    while len(num_list) > 0:
-        min_value = num_list[0]
-        for num in num_list:
-            if num < min_value:
-                min_value = num
-        result_list.append(min_value)
-        num_list.remove(min_value)
+    while True:
+        if len(x) == 0 or len(y) == 0:
+            for term in x:
+                result_list.append(term)
+            for term in y:
+                result_list.append(term)
+            break
+        x_term = x[0]
+        y_term = y[0]
+        if x_term < y_term:
+            result_list.append(x_term)
+            x.pop(0)
+        elif y_term <= x_term:
+            result_list.append(y_term)
+            y.pop(0)
     return result_list
 
 
@@ -19,25 +27,11 @@ def merge_sort(input_list):
     for j in input_list:
         second_half.append(j)
     if len(first_half) == 1 or len(second_half) == 1:
-        '''
-        if first_half[0] < second_half[0]:
-            merged_list.append(first_half[0])
-            merged_list.append(second_half[0])
-        elif second_half[0] <= first_half[0]:
-            merged_list.append(second_half[0])
-            merged_list.append(first_half[0])
-        '''
-        for i in second_half:
-            first_half.append(i)
-        merged_list = sort(first_half)
-        return merged_list
+        return sort(first_half, second_half)
     elif len(first_half) != 1 or len(second_half) != 1:
         sorted_first_half = merge_sort(first_half)
         sorted_second_half = merge_sort(second_half)
-        for i in sorted_second_half:
-            sorted_first_half.append(i)
-        final_merged_list = sort(sorted_first_half)
-        return final_merged_list
+        return sort(sorted_first_half, sorted_second_half)
 
 print("Testing 'merge_sort'...")
 assert merge_sort([4,8,7,7,4,2,3,1]) == [1, 2, 3, 4, 4, 7, 7, 8]
