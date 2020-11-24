d0f2a3898bb60c07482eda9dd3925b15e285f7c1
JHpusd <justinhongdebate@gmail.com>
2020-11-24 01:06:12 +0000

    made cartesian product function

---
+++ assignment_34/cartesian_product.py
@@ -0,0 +1,16 @@
+def cartesian_product(arrays):
+    points = [[]]
+    for arr in arrays:
+        for i in range(len(points)):
+            for element in arr:
+                temp_point = list(points[0])
+                temp_point.append(element)
+                points.append(temp_point)
+            points.remove(points[0])
+    return points
+
+print("Testing function 'cartesian_product'...")
+assert cartesian_product([['a'], [1,2,3], ['Y','Z']]) == [
+    ['a',1,'Y'], ['a',1,'Z'], ['a',2,'Y'], ['a',2,'Z'], [
+        'a',3,'Y'], ['a',3,'Z']]
+print("PASSED")
