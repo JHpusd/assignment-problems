7ccf9abae7750f79c7396c82d05cf937e7a87d3b
JHpusd <justinhongdebate@gmail.com>
2021-02-10 01:37:07 +0000

    finished firstNumberWithChainLengthGreaterThan function (haskell)

---
+++ assignment_56/collatz.hs
@@ -0,0 +1,10 @@
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]  
+chain n
+    | even n =  n:chain (n `div` 2)  
+    | odd n  =  n:chain (n*3 + 1)
+
+chainLengthList n = takeWhile (<n) (map (length . chain) [1..])
+firstNumberWithChainLengthGreaterThan n = length(chainLengthList n) + 1
+
+main = print(firstNumberWithChainLengthGreaterThan 15)
