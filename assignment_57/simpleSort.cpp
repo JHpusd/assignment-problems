894f869f9ea0fd43f713019a725cae37e9618e18
JHpusd <justinhongdebate@gmail.com>
2021-02-14 19:24:29 +0000

    finished simple sort for c++

---
+++ assignment_57/simpleSort.cpp
@@ -0,0 +1,34 @@
+# include <iostream>
+# include <cassert>
+int main()
+{
+    int arr[5]{ 30, 50, 20, 10, 40 };
+
+    int counter = 0;
+
+    for (int n = 0; n <= 4; n++){
+        int smallestVal = arr[counter];
+        int smallestIndex = counter;
+        for (int i = counter; i <= 4; i++){
+            if (arr[i] < smallestVal){
+                smallestIndex = i;
+                smallestVal = arr[i];
+            }
+        }
+        arr[smallestIndex] = arr[counter];
+        arr[counter] = smallestVal;
+        counter += 1;
+    }
+
+    std::cout << "Testing...\n";
+
+    assert(arr[0]==10);
+    assert(arr[1]==20);
+    assert(arr[2]==30);
+    assert(arr[3]==40);
+    assert(arr[4]==50);
+
+    std::cout << "Succeeded";
+
+    return 0;
+}
